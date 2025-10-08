39250739f9f98ad48a77de298c935208c6c27712
amaljose-aj <57029243+amaljose-aj@users.noreply.github.com>
2025-10-08 11:16:32 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "checkout.h"
+#include "PaymentType.h"
+
+int main() {
+    double amount = 150.75;
+
+    checkout(PaymentMode::PayPal, amount);
+    checkout(PaymentMode::GooglePay, amount);
+    checkout(PaymentMode::CreditCard, amount);
+    checkout(PaymentMode::Unknown, amount);
+
+    return 0;
+}
