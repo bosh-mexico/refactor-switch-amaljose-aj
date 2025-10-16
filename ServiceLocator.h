2a156e0179ee3ee2556f0df3f1dddd0d7fa696bf
amaljose-aj <57029243+amaljose-aj@users.noreply.github.com>
2025-10-16 10:54:32 +0530

    Update ServiceLocator.h

--- ServiceLocator.h
+++ ServiceLocator.h
@@ -1,13 +1,32 @@
-#ifndef SERVICELOCATOR_H
-#define SERVICELOCATOR_H
+#ifndef SERVICE_LOCATOR_H
+#define SERVICE_LOCATOR_H
 
-#include "handlers.h"
+#include <map>
+#include <functional>
 
+template<typename Key, typename Handler>
 class ServiceLocator {
 public:
-    static void processPayment(void (*handler)(double), double amount) {
-        handler(amount);
+    static ServiceLocator& instance() {
+        static ServiceLocator locator;
+        return locator;
     }
+
+    void registerHandler(Key key, Handler handler) {
+        handlers[key] = handler;
+    }
+
+    Handler getHandler(Key key, Handler defaultHandler) const {
+        auto it = handlers.find(key);
+        if (it != handlers.end()) {
+            return it->second;
+        }
+        return defaultHandler;
+    }
+
+private:
+    std::map<Key, Handler> handlers;
+    ServiceLocator() = default;
 };
 
 #endif
