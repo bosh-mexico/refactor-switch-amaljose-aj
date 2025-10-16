30cb0177437433010b771c8aad8d818930aa5dce
amaljose-aj <57029243+amaljose-aj@users.noreply.github.com>
2025-10-16 10:51:58 +0530

    Update main.cpp
    updated testcase

--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
-#include "checkout.h"
+#include <iostream>
 #include "PaymentType.h"
+#include "ServiceLocator.h"
+#include "handlers.h"
+#include "checkout.h"
+#include <functional>
 
 int main() {
-    double amount = 150.75;
+    using Handler = std::function<std::string(double)>;
+    auto& locator = ServiceLocator<PaymentType, Handler>::instance();
 
-    checkout(PaymentMode::PayPal, amount);
-    checkout(PaymentMode::GooglePay, amount);
-    checkout(PaymentMode::CreditCard, amount);
-    checkout(PaymentMode::Unknown, amount);
+    locator.registerHandler(PaymentType::PayPal, handlePayPal);
+    locator.registerHandler(PaymentType::GooglePay, handleGooglePay);
+    locator.registerHandler(PaymentType::CreditCard, handleCreditCard);
+
+    std::cout << checkout(PaymentType::PayPal, 150.0) << "\n";
+    std::cout << checkout(PaymentType::GooglePay, 200.0) << "\n";
+    std::cout << checkout(PaymentType::CreditCard, 300.0) << "\n";
+    std::cout << checkout(PaymentType::Unknown, 400.0) << "\n";
+    std::cout << checkout(PaymentType::PayPal, -10.0) << "\n";
 
     return 0;
 }
